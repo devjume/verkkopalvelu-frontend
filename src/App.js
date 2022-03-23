4a2518fb70751bc467496a30b24b0d66ccd30e28
Jume <devjume@mailbox.org>
2022-03-23 22:59:59 +0200

    Lisätty esimerkki tuotteiden näyttämisestä

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+const URL = "http://localhost/verkkopalvelu-backend";
+
 export default function App() {
   return (
     <>
@@ -14,7 +16,7 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} ></Route>
-          <Route path="/products" element={<Products />} ></Route>
+          <Route path="/products" element={<Products url={URL}/>}  ></Route>
         </Routes>
         <Header></Header>
         <Footer />
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from "react"
 
-export default function Products() {
+export default function Products({url}) {
+
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [shopList, setShopList] = useState(null);
+  const [products, setProducts] = useState(null);  
+
+  async function fetchProducts() {
+    try {
+      const response = await fetch(`${url}/products.php`)
+      const data = await response.json();
+
+      setProducts(data);
+      setFetchError(null);
+    } catch (error) {
+      setFetchError(error.message);
+    } finally {
+      setIsLoaded(true);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Products:</h1>
+      <ul>
+          {/* Jos tietoja ei ole vielä ladattu, näytä käyttäjälle viesti */}
+          {!isLoaded && "Tietoja ladataan..."}
+          
+          {/* Jos virheitä, näytä ne käyttäjälle */}
+          {fetchError && {fetchError}}
+
+          {/* Kun tieto on ladattu eikä virheitä ole niin näytä haluttu elementti */}
+          {!fetchError && isLoaded && products.map((product) => <li key={product.tuote_id}>{product.tuotenimi}</li>)}
+          
+      </ul>
     </div>
   )
 }
