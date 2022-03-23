34750727a377e17d9d8ff7db6ef958bfb030072e
Jume <devjume@mailbox.org>
2022-03-23 23:32:23 +0200

    Lisätty virheilmoitus esimerkki

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,12 +5,14 @@ export default function Products({url}) {
   const [fetchError, setFetchError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [shopList, setShopList] = useState(null);
-  const [products, setProducts] = useState(null);  
+  const [products, setProducts] = useState([]);  
 
   async function fetchProducts() {
     try {
       const response = await fetch(`${url}/products.php`)
       const data = await response.json();
+
+      if (!response.ok) throw Error(data.error);
 
       setProducts(data);
       setFetchError(null);
@@ -30,14 +32,14 @@ export default function Products({url}) {
       <h1>Products:</h1>
       <ul>
           {/* Jos tietoja ei ole vielä ladattu, näytä käyttäjälle viesti */}
-          {!isLoaded && "Tietoja ladataan..."}
+          {!isLoaded && <li>"Tietoja ladataan..."</li>}
           
           {/* Jos virheitä, näytä ne käyttäjälle */}
-          {fetchError && {fetchError}}
+          {fetchError && <li>{fetchError}</li>}
 
           {/* Kun tieto on ladattu eikä virheitä ole niin näytä haluttu elementti */}
-          {!fetchError && isLoaded && products.map((product) => <li key={product.tuote_id}>{product.tuotenimi}</li>)}
-          
+          {isLoaded && !fetchError && (products.map((product) => <li key={product.tuote_id}>{product.tuotenimi}</li>))}
+        
       </ul>
     </div>
   )
