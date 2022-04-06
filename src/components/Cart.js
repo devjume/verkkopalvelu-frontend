56f1928ee8390f91dec9d1f48377bd7b73a86520
Jume <devjume@mailbox.org>
2022-04-06 11:06:38 +0300

    Korjattu class name bugi

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function Cart({cart}) {
     return(
         <Link to="/order" className="nav-link">
-        <i class="bi bi-cart-fill"></i>
+        <i className="bi bi-cart-fill"></i>
           <span>{cart.length}</span>
         </Link>
     )
