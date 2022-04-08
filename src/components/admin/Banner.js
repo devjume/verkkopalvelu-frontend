b19b9733e4cd32a2753b4a055ae36d478cc35c87
Veeti Oivo <MY_NAME@example.com>
2022-04-08 11:25:43 +0300

    Banner toimii mutta kuva/ulkoasu pitää vielä uusia

--- src/components/admin/Banner.js
+++ src/components/admin/Banner.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 export default function Banner() {
   return (
     <div id='banner'>
-        <a href="../discount">
-         <img alt="discount" id='bannerimg' src="../TestiBanner.png"/>
-      </a>
+      <Link to="/discount" className="nav-link">
+        <img alt="discount" id='bannerimg' src="../TestiBanner.png"/>
+        </Link>
     </div>
   )
 }
