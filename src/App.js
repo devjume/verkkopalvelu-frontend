428ed55e39e761ba9c3439c31c46c8073b1c2ef9
Veeti Oivo <MY_NAME@example.com>
2022-04-12 10:05:27 +0300

    Nyt voi laittaa ostoskoriin aletuote sivultakin

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ export default function App() {
           <Route path="/contact" element={<Contact url={URL} />}></Route>
           <Route path="/Order" element={<Order cart={cart} removeFromCart={removeFromCart} />} />
           
-          <Route path="/discount" element={<Discount url={URL}/>}></Route>
+          <Route path="/discount" element={<Discount url={URL} addToCart={addToCart}/>}></Route>
           <Route path="/product/:id" element={<Product url={URL} />}></Route>
           <Route path="/product/carousel/:id" element={<CarDetails url={URL} addToCart={addToCart} />}></Route>
         </Routes>
