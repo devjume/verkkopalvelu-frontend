cfab79121031b733eb63b8fe8f0838f582e6aa76
Jume <devjume@mailbox.org>
2022-04-15 18:28:02 +0300

    Alehinta näkyy  nyt myös tuotesivulla

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,6 +7,8 @@ import { Link, useParams } from "react-router-dom";
 export default function Product({url}) {
 
   const [product, setProduct] = useState(undefined)
+  const [discountPrice, setDiscountPrice] = useState(null);
+
   const {id} = useParams();
 
 
@@ -14,9 +16,17 @@ export default function Product({url}) {
       axios.get(`${url}/product.php`, { params: {id: id}})
         .then((response) => {
           setProduct(response.data);
+
+          // Tarkastaa onko alehintaa määritelty. Jos alehintaa ei ole tai se on 0 niin laitetaan discountPrice arvoksi = null. Tämä arvo nyt yksinkertaisempi tarkistaa alhaalla return kohdassa.
+          if (isNaN(parseFloat(response.data.alehinta)) || parseFloat(response.data.alehinta) <= 0) {
+            setDiscountPrice(null);
+          } else {
+            setDiscountPrice(parseFloat(response.data.alehinta));
+          }
         }).catch(error => {
           alert(error.response ? error.response.data.error : error)
         })
+
 
   }, [])
 
@@ -27,21 +37,18 @@ export default function Product({url}) {
 
   return (
     <>
-      
       <div className='row mt-4 px-4'>
-        <h3>{product.tuotenimi}</h3>
         <div className='col-4 mt-3'>
           <img src={product.kuvatiedosto} className="" alt={product.tuotenimi} style={{ width: "auto", height: "350px", objectFit: "scale-down" }}></img>
         </div>
         <div className='col-4 offset-2 mt-3'>
           <h4>{product.tuotenimi}</h4>
           <h6>Valmistaja: {product.valmistaja}</h6>
-          <h3>{product.hinta} €</h3>
+          <h3>{discountPrice === null ? product.hinta : discountPrice}€ {discountPrice !== null && <span className="linethrough fw-normal">{product.hinta}€</span>}</h3>
         </div>
       </div>
       <div className='row mt-4 px-4'>
         <div className='col-6 mt-3'>
-          
           <h4>Kuvaus:</h4>
           <p>{product.kuvaus}</p>
         </div>
