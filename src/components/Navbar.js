8342920d1ddf2bb4045a3f1dbc05c348fff8676b
ronikauppinen <80982932+ronikauppinen@users.noreply.github.com>
2022-04-19 13:54:52 +0300

    navbar kuva png

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar({categories, cart}) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link to={`/`}  className="nav-link navbar-brand" aria-current="page"><img className='navbarimg' src='/Screenshot (58).png'></img></Link>
+        <Link to={`/`}  className="nav-link navbar-brand" aria-current="page"><img className='navbarimg' src='/kuvapng.png'></img></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
